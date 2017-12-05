264bb384e82a98b3a3d74d0e050f5384a0b6aee7
Quentin Laplanche <quentin.laplanche@outlook.fr>
2017-12-05 22:36:04 +0100

    Change AppBar to a stateful component for routing

--- src/components/AppBarDFS.jsx
+++ src/components/AppBarDFS.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import AppBar from 'material-ui/AppBar';
 import {red900} from 'material-ui/styles/colors'
 
@@ -6,16 +6,23 @@ import FlatButton from 'material-ui/FlatButton'
 
 import Logo from '../img/DFS.png';
 
+class AppBarDFS extends Component {
+  constructor(props){
+    super(props);
+  }
 
-const AppBarDFS = () => (
-  <AppBar 
-    style={{backgroundColor: '#780003'}}
-    iconClassNameLeft="play_arrow"
-    iconElementRight={<FlatButton label="Voir le site de la Nuit" disabled />}
-    title="Team DFS;"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-    zDepth={2}
-  />
-);
+  render(){
+    return(
+      <AppBar 
+      style={{backgroundColor: '#780003'}}
+      iconClassNameLeft="play_arrow"
+      iconElementRight={<FlatButton label="Voir le site de la Nuit" disabled />}
+      title={<span>Team DFS;</span>}
+      iconClassNameRight="muidocs-icon-navigation-expand-more"
+      zDepth={2}
+    />
+    );
+  }
+}
 
 export default AppBarDFS;
