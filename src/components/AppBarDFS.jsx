78d9abfc1883c3f488aae62e1365c0116478066f
Quentin Laplanche <quentin.laplanche@outlook.fr>
2017-12-05 18:27:11 +0100

    Get the right red color from the logo

--- src/components/AppBarDFS.jsx
+++ src/components/AppBarDFS.jsx
@@ -9,11 +9,12 @@ import Logo from '../img/DFS.png';
 
 const AppBarDFS = () => (
   <AppBar 
-    style={{backgroundColor: red900}}
+    style={{backgroundColor: '#780003'}}
     iconClassNameLeft="play_arrow"
     iconElementRight={<FlatButton label="Voir le site de la Nuit" disabled />}
     title="Team DFS;"
     iconClassNameRight="muidocs-icon-navigation-expand-more"
+    zDepth={2}
   />
 );
 
