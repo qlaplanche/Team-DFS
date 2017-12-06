22804c3d3e6fdc57b371ea039b3b9c0d97f494d5
Quentin Laplanche <quentin.laplanche@outlook.fr>
2017-12-06 10:04:16 +0100

    Redirection for home page

--- src/components/AppBarDFS.jsx
+++ src/components/AppBarDFS.jsx
@@ -3,10 +3,19 @@ import AppBar from 'material-ui/AppBar';
 
 import FlatButton from 'material-ui/FlatButton'
 
+const styles = {
+  title: {
+    cursor: 'pointer',
+  },
+};
 
 class AppBarDFS extends Component {
   constructor(props){
     super(props);
+  }
+
+  handleRedirection() {
+   window.location = '/dfs' 
   }
 
   render(){
@@ -15,7 +24,8 @@ class AppBarDFS extends Component {
       style={{backgroundColor: '#780003'}}
       iconClassNameLeft="play_arrow"
       iconElementRight={<FlatButton label="Voir le site de la Nuit" disabled />}
-      title={<span>Team DFS;</span>}
+      title={<span style={styles.title}>Team DFS;</span>}
+      onTitleClick={this.handleRedirection}
       iconClassNameRight="muidocs-icon-navigation-expand-more"
       zDepth={2}
     />
